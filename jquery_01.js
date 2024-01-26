e30d3ef52d291f7bf57ae6832f81f95c94445304
babybee03 <flowerysora@hanmail.net>
2024-01-26 18:08:17 +0900

    jquery practice

--- jquery_01.js
+++ jquery_01.js
@@ -1,66 +1,31 @@
-/*
 $(function(){
-        var obj = [
-                {"area" : "서울"},
-                {"area" : "부산"},
-                {"area" : "전주"}
-        ];
-        $(obj).each(function(i, object){
-                console.log(i);
-                console.log(object)
-        });
-        console.log("====The End 1 ====");
-        
-})
-*/
+        $(window).scroll(function(){
+                var scroll_height = $(window).scrollTop();
+                console.log(scroll_height);
 
-$(function(){
-        var duration = 500;
-        /*button 1행*/
-        $('#button_all button:nth-child(-n+4)')
-        .on('mouseover',function(){
-                $(this).stop(true).animate({
-                        backgroundColor: "#ae5e9b",
-                        color: '#fff'
-                }, duration);
+                if(scroll_height >= 400) {
+                        $("#box_02").animate({
+                                "left" : "0",
+                                "opacity" : "1"
+                        }, 1000)
+                }
+                if(scroll_height >= 1000) {
+                        $("#box_03").animate({
+                                "left" : "0",
+                                "opacity" : "1"
+                        }, 1000)
+                }
+                if(scroll_height >= 1400) {
+                        $("#box_04").animate({
+                                "left" : "0",
+                                "opacity" : 1
+                        }, 1000)
+                }
+                if(scroll_height >= 2000) {
+                        $("#box_05").animate({
+                                "left":"0",
+                                "opacity": 1
+                        },1000)
+                }
         })
-        .on('mouseout', function(){
-                $(this).stop(true).animate({
-                        backgroundColor: "#fff",
-                        color: '#ebc000'
-                }, duration);
-        });
-
-         /*button 2행*/
-         $('#button_all button:nth-child(n+5):nth-child(-n+8)')
-         .on('mouseover',function(){
-                 $(this).stop(true).animate({
-                        borderWidth: '10px',
-                        lineHeight: '60px',
-                         color: '#ae5e9b'
-                 }, duration);
-         })
-         .on('mouseout', function(){
-                 $(this).stop(true).animate({
-                        borderWidth: '0px',
-                         color: '#ebc000'
-                 }, duration);
-         })
-
-          /*button 3행*/
-          $('#button_all button:nth-child(n+9)')
-          .on('mouseover',function(){
-                  $(this).stop(true).animate({
-                        'width': '300px',
-                        'backgroundColor': '#333',
-                        'color': '#ddd'
-                  }, duration);
-          })
-          .on('mouseout', function(){
-                  $(this).stop(true).animate({
-                        'width': '200px',
-                        'backgroundColor': '#fff',
-                        'color': '#333'
-                  }, duration);
-          })
 })

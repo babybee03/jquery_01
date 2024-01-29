4e938759c002d9e81949eb0aa8b9f89412afc728
babybee03 <flowerysora@hanmail.net>
2024-01-29 16:36:22 +0900

    jquery practice

--- jquery_01.js
+++ jquery_01.js
@@ -1,19 +1,16 @@
+
 $(function(){
-    var obj = [
-        {"area":"서울"},
-        {"area":"부산"},
-        {"area":"전주"}
-    ];
-    $("#menu2 li").each(function(i, object){
-        console.log(i);
-        console.log(object);
-    })
-
-    $.each(obj, function(i, object) {
-        console.log(i + ":", object);
-    })
-
-    console.log("====The End====")
+    $(".btn1").on("click",function(e) {
+        e.preventDefault();
+        $(".txt1")
+        .css({"background-color":"#ff0"});
+    });
+    $(".btn2").on("click",function(e) {
+        $(".txt2")
+        .css({"background-color":"#0ff"});
+    });
+    $(".btn3").on("dbclick",function() {
+        $(".txt3")
+        .css({"background-color":"#0f0"});
+    });
 })
-
-console.log("====The End 2====")

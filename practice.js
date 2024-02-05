19ee1c4d023aea94ed2687cab0345e447f609bb2
babybee03 <flowerysora@hanmail.net>
2024-02-05 12:13:16 +0900

    slide

--- practice.js
+++ practice.js
@@ -1,20 +1,62 @@
 $(function(){
-    $("#wrap a[target]").css({
-        "color":"#f00"
-    });
-    $("#wrap a[href ^= 'https']").css({
-        "color":"#0f0"
-    });
-    $("#wrap a[href $= 'net']").css({
-        "color":"#00f"
-    });
-    $("#wrap a[href *= 'google']").css({
-        "color":"#000"
-    });
-    $("#member_f :text").css({
-        "background-color": "#ff0"
-    });
-    $("#member_f :password").css({
-        "background-color": "#0ff"
-    });
-});
+    $photo_all =$(".photo_all");
+    $photo_all_list = $(".photo_all > div");
+    $dot_all = $(".dot_all");
+    $(dot_all_list) = $(".dot_all > div");
+
+    function var_set() {
+        photo_width = $(".photo").width();
+        index_now = $(".dot_all > div.open").index();
+        dot_count = $dot_all_list.length;
+    }
+
+    function dot_click() {
+        $(".dot_all > div").on("click", function(){
+            index_now = $(this).index();
+            slide_move();
+        })
+    }
+
+    function slide_move() {
+        $(".photo_all").animate({
+            left: -(photo_width * index_now)
+        }, 2000, "swing")
+
+        $dot_all_list.removeClass("open");
+        $dot_all_list.eq(index_now).addClass("open");
+    }
+
+    $(".prev").on("click", function(){
+        prev_move();
+    })
+    function prev_move() {
+        if(index_now == 0) {
+            index_now = dot_count -1;
+        } else {
+            index_now--;
+        }
+        slide_move();
+    }
+    $(".next").on("click", function(){
+        next_move();
+    })
+
+    function next_move() {
+        if(index_now == dot_count -1) {
+            index_now = 0;
+        } else {
+            index_now++;
+        }
+        slide_move();
+    }
+    function start_timer() {
+        timer = setInterval(next_move, 3000);
+    }
+    function stop_timer() {
+        clearInterval(timer);
+    }
+    $(".photo_wrap").hover(stop_timer, start_timer);
+    var_set();
+    dot_click();
+    start_timer();
+})

d7a34d473b84c3871b0c5d84d345707090060553
babybee03 <153714246+babybee03@users.noreply.github.com>
2024-02-08 00:31:43 +0900

    practice

--- practice.js
+++ practice.js
@@ -1,13 +1,15 @@
+/*
 $(function(){
-    $photo_all = $(".photo_all");
-    $photo_all_list = $(".photo_all > div");
-    $dot_all = $(".dot_all");
-    $dot_all_list = $(".dot_all > div");
+    photo_all = $(".photo_all");
+    photo_all_list = $(".photo_all > div");
+    dot_all = $(".dot_all");
+    dot_all_list = $(".dot_all > div");
+    timer = null;
 
     function var_set() {
         photo_width = $(".photo").width();
         index_now = $(".dot_all > div.open").index();
-        dot_count = $dot_all_list.length;
+        dot_count = dot_all_list.length;
     }
 
     function dot_click() {
@@ -22,8 +24,8 @@ $(function(){
             left: -(photo_width * index_now)
         }, 2000, "swing")
 
-        $dot_all_list.removeClass("open");
-        $dot_all_list.eq(index_now).addClass("open");
+        dot_all_list.removeClass("open");
+        dot_all_list.eq(index_now).addClass("open");
     }
 
     $(".prev").on("click", function(){
@@ -58,5 +60,36 @@ $(function(){
     $(".photo_wrap").hover(stop_timer, start_timer);
     var_set();
     dot_click();
+    start_timer();
+})
+*/
+$(function(){
+    photo_all = $(".photo_all");
+    photo_all_list = $(".photo_all > div");
+    timer = null;
+
+    function var_set() {
+        photo_width = $(".photo").width();
+        index_now = 0;
+        photo_count = photo_all_list.length;
+    }
+
+    function slide_move() {
+        $(".photo_all").animate({
+            left: -(photo_width * index_now)
+        }, 2000, "swing")
+    }
+    function next_move() {
+        if(index_now == photo_count -1) {
+            index_now = 0;
+        } else {
+            index_now++;
+        }
+        slide_move();
+    }
+    function start_timer() {
+        timer = setInterval(next_move, 3000);
+    }
+    var_set();
     start_timer();
 })
